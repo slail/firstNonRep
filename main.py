23412bad02c309914174560eeb232a92a2b9f90e
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-28 12:17:33 -0400

    added algorithm

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ def firstNonRepeatingCharacter(string):
     string's first non-repeating character. The first non-repeating character is the first character in a string that
     occurs only once. If the input string doesn't have any non-repeating characters, your function should return -1.
     """
+    character_frequency = {}
+    for character in string:
+        character_frequency[character] = character_frequency.get(character, 0) + 1
+
+    for index in range(len(string)):
+        character = string[index]
+        if character_frequency[character] == 1:
+            return index
     return -1
+
+
+print(firstNonRepeatingCharacter("abcdaef"))
